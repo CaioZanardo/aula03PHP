d536692696e45335189819bdfe753ab577b7993d
Caio Zanardo <55753694+CaioZanardo@users.noreply.github.com>
2023-03-02 20:14:34 -0300

    Exemplo
    Exemplo utilizado para criar formulario

---
+++ exemploPOST.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <form action = "exemploPOST.php" method="POST">
+            Nome: <input type = "text" name = "t_nome"/>
+            Idade: <input type = "text" name = "t_idade"/>
+            <input type="submit"/>
+        </form>
+        <h1> Dados informados:</h1>
+        <?php
+            if( isset($_POST["t_name"]) || isset($_POST["t_idade"]) ){
+            echo "Olá " . $_POST['t_nome']. "<br/>";
+            echo "Você tem " . $_POST['t_idade']."anos.";
+            }else{
+                echo "Nenhum dado informado";
+            }
+        ?>
+    </body>
+</html>
